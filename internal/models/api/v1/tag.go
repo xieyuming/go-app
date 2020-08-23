b8fdb9b3e2fdcca67d1326858710228338aa6397
xieyuming <xym283@126.com>
2020-08-23 22:48:57 +0800

    新方法提交

--- internal/models/api/v1/tag.go
+++ internal/models/api/v1/tag.go
@@ -1,78 +1,29 @@
 package v1
 
-import (
-	"github.com/astaxie/beego/validation"
-	models2 "github.com/frankie/go-app/internal/models"
-	"github.com/frankie/go-app/pkg/ecode"
-	"github.com/frankie/go-app/pkg/setting"
-	"github.com/frankie/go-app/pkg/util"
-	"github.com/gin-gonic/gin"
-	"github.com/unknwon/com"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
-//获取多个文章标签
-func GetTags(c *gin.Context) {
-	name := c.Query("name")
-	maps := make(map[string]interface{})
-	data := make(map[string]interface{})
+type Tag struct{}
 
-	if name != "" {
-		maps["name"] = name
-	}
-
-	var state int = -1
-	if arg := c.Query("state"); arg != "" {
-		state = com.StrTo(arg).MustInt()
-		maps["state"] = state
-	}
-
-	code := ecode.SUCCESS
-
-	data["lists"] = models2.GetTags(util.GetPage(c), setting.PageSize, maps)
-	data["total"] = models2.GetTagTotal(maps)
-
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg":  ecode.GetMsg(code),
-		"data": data,
-	})
+func NewTag() Tag {
+	return Tag{}
 }
 
-//新增文章标签
-func AddTag(c *gin.Context) {
-	name := c.Query("name")
-	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
-	createdBy := c.Query("created_by")
+func (t Tag) Get(c *gin.Context) {
 
-	valid := validation.Validation{}
-	valid.Required(name, "name").Message("名称不能为空")
-	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
-	valid.Required(createdBy, "created_by").Message("创建人不能为空")
-	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
-	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
-
-	code := ecode.INVALID_PARAMS
-	if !valid.HasErrors() {
-		if !models2.ExistTagByName(name) {
-			code = ecode.SUCCESS
-			models2.AddTag(name, state, createdBy)
-		} else {
-			code = ecode.ERROR_EXIST_TAG
-		}
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg":  ecode.GetMsg(code),
-		"data": make(map[string]string),
-	})
 }
 
-//修改文章标签
-func EditTag(c *gin.Context) {
+func (t Tag) List(c *gin.Context) {
+
 }
 
-//删除文章标签
-func DeleteTag(c *gin.Context) {
+func (t Tag) Delete(c *gin.Context) {
+
+}
+
+func (t Tag) Create(c *gin.Context) {
+
+}
+
+func (t Tag) Update(c *gin.Context) {
+
 }
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -35,18 +35,18 @@ func NewRouter() *gin.Engine {
 
 	apiv1 := r.Group("/api/v1")
 	{
-		apiv1.GET("/tags")
-		apiv1.POST("/tags")
-		apiv1.PUT("/tags/:id")
-		apiv1.DELETE("/tags/:id")
-		apiv1.PATCH("/tags/:id/state")
+		apiv1.GET("/tags", v1.NewTag().Get)
+		apiv1.POST("/tags", v1.NewTag().Create)
+		apiv1.PUT("/tags/:id", v1.NewTag().Update)
+		apiv1.DELETE("/tags/:id", v1.NewTag().Delete)
+		apiv1.PATCH("/tags/:id/state", v1.NewTag().Update)
 
-		apiv1.GET("/articles")
-		apiv1.GET("/articles/:id")
-		apiv1.POST("/articles")
-		apiv1.PUT("/articles/:id")
-		apiv1.DELETE("/articles/:id")
-		apiv1.PATCH("/articles/:id/state")
+		apiv1.GET("/articles", v1.NewArticle().Get)
+		apiv1.GET("/articles/:id", v1.NewArticle().List)
+		apiv1.POST("/articles", v1.NewArticle().Create)
+		apiv1.PUT("/articles/:id", v1.NewArticle().Update)
+		apiv1.DELETE("/articles/:id", v1.NewArticle().Delete)
+		apiv1.PATCH("/articles/:id/state", v1.NewArticle().Update)
 	}
 
 	return r
