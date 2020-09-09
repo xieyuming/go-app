0c0418b7fa2448aa43e3e6bdaf97998fdd73ac7c
xieyuming <88045637@qq.com>
2020-09-09 17:37:48 +0800

    模块代码提交

--- internal/models/tag.go
+++ internal/models/tag.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/frankie/go-app/pkg/app"
+import (
+	"github.com/frankie/go-app/pkg/app"
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	*Model
@@ -15,4 +18,16 @@ func (a Tag) TableName() string {
 type TagSwagger struct {
 	List  []*Tag
 	Pager *app.Pager
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.Status)
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+	return count, nil
 }
